9103e8dc0f3bb60261379de2c760448aa0b92026
Neha Chhillar <your.email@example.com>
2025-01-26 06:17:55 +0530

    Fix escaped apostrophes in About page

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
+    domains: ['vercel.com'],
     unoptimized: true,
-  },
-  ...(process.env.NODE_ENV === 'production' ? {
-    assetPrefix: '/nc-portfolio/',
-    basePath: '/nc-portfolio',
-    trailingSlash: true,
-  } : {
-    basePath: '',
-  }),
+  }
 }
 
 module.exports = nextConfig 
