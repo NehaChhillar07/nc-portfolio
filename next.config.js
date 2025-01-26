cf422c6949fffd73e445a64aa1dc4c723853baa7
Neha Chhillar <your.email@example.com>
2025-01-26 06:18:41 +0530

    Add ESLint config and update Next.js config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['vercel.com'],
-    unoptimized: true,
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig 
