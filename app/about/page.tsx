a635f1b875aa9148f70da13ecb4356105728240c
Neha Chhillar <your.email@example.com>
2025-01-27 12:19:20 +0530

    add pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,34 +13,34 @@ export default function About() {
             <div className="w-[485px] h-[558px] relative">
               <AnimatedImageGallery />
             </div>
-            <div className="flex flex-col gap-[80px]">
-              <div className="flex flex-col pt-[40px] gap-6 w-full">
-                <h1 className="text-[42px] font-bold leading-[52px] text-[#161C24]">
-                  Hello,
-                </h1>
-                <div className="font-['Futura'] font-medium text-[18px] leading-[32px] text-[#212B36] flex flex-col gap-8">
-                  <p>
-                    Thanks for dropping by, Neha Chhillar&apos;s profile. She crafts simple, effective designs that resonate with users and meet business goals. In high-pressure situations, her calm approach harmonizes with tight deadlines and complex team dynamics.
-                  </p>
-                  <p>
-                    Outside of design, spending time with family fills her with joy and inspiration, which she carries into her work. This harmony of personal happiness and professional dedication nourishes her creativity.
-                  </p>
-                  <p>
-                    She loves to travel and learn about different cultures. It helps her bring new ideas and perspectives into her work.
-                  </p>
-                  <p>
-                    Let&apos;s use these inspirations to create designs that speak to everyone!
-                  </p>
-                </div>
+            <div className="flex flex-col pt-[40px] gap-6 w-[847px]">
+              <h1 className="text-[42px] font-bold leading-[52px] text-[#161C24]">
+                Hello,
+              </h1>
+              <div className="w-[847px] font-['Futura'] font-medium text-[18px] leading-[32px] text-[#212B36] flex flex-col gap-8">
+                <p>
+                  Thanks for dropping by, Neha Chhillar's profile. She crafts simple, effective designs that resonate with users and meet business goals. In high-pressure situations, her calm approach harmonizes with tight deadlines and complex team dynamics.
+                </p>
+                <p>
+                  Outside of design, spending time with family fills her with joy and inspiration, which she carries into her work. This harmony of personal happiness and professional dedication nourishes her creativity.
+                </p>
+                <p>
+                  She loves to travel and learn about different cultures. It helps her bring new ideas and perspectives into her work.
+                </p>
+                <p>
+                  Let's use these inspirations to create designs that speak to everyone!
+                </p>
               </div>
-              <div className="w-[485px] h-[485px] relative">
-                <Image
-                  src="/images/about-office.png"
-                  alt="Office environment"
-                  className="rounded-[18px] object-cover"
-                  fill
-                />
-              </div>
+            </div>
+          </div>
+          <div className="flex justify-center w-[847px] ml-[565px]">
+            <div className="w-[485px] h-[485px] relative">
+              <Image
+                src="/images/about-office.png"
+                alt="Office environment"
+                className="rounded-[18px] object-cover"
+                fill
+              />
             </div>
           </div>
         </section>
--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    unoptimized: true,
   },
+  ...(process.env.NODE_ENV === 'production' ? {
+    assetPrefix: '/nc-portfolio/',
+    basePath: '/nc-portfolio',
+    trailingSlash: true,
+  } : {
+    basePath: '',
+  }),
 }
 
 module.exports = nextConfig 
